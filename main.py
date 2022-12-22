bffea691457e1e56f75fae6fda9d1b1b7cbddb70
Igor <igor.gorobets2013@yandex.ru>
2022-12-22 15:14:00 +0300

    Second commit

--- main.py
+++ main.py
@@ -140,7 +140,7 @@ def google_table(dict_cards):
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    SERVICE_ACCOUNT_FILE = os.path.join(BASE_DIR, '../parser_2/credentials.json')
+    SERVICE_ACCOUNT_FILE = os.path.join(BASE_DIR, 'credentials.json')
 
     credentials = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
