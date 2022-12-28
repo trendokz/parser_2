e966d5b4f6da7d6ff98b7035be65348bf0f7a3ef
Igor <igor.gorobets2013@yandex.ru>
2022-12-28 19:36:49 +0300

    Seventh commit

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ def google_table(dict_cards):
 
     # mail bot 'parsers@parsers-372008.iam.gserviceaccount.com'
     SAMPLE_SPREADSHEET_ID = '107SdHe8_dV6npe_dKE-7xA2QJgxz6ZOywOy-GZyrZX0'
-    SAMPLE_RANGE_NAME = 'komfort.kz'
+    SAMPLE_RANGE_NAME = 'komfort.kz!A1:C'
 
     try:
         service = build('sheets', 'v4', credentials=credentials).spreadsheets().values()
